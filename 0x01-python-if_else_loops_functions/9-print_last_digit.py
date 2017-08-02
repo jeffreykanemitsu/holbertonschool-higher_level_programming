f79b174e77afbdd845362b72ac5fc5e70100c3b3
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-02 05:39:04 +0000

    task 9: removed test code

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,8 +5,3 @@ def print_last_digit(number):
     digit = number % 10
     print("{:d}".format(digit), end="")
     return (digit)
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
