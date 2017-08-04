2886fd54b828ab190389c91d17a625ee3d766eea
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-04 07:24:19 +0000

    task 4: changed slice from 0 to 1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     LOL = dir(hidden_4)
     for x in LOL:
-        if x[:0] != "_":
+        if x[:1] != "_":
             print("{:s}".format(x), end="\n")
