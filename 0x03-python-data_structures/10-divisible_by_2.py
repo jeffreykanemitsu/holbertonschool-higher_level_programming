c61c4ad93948b6491fceb28bb41b652c401e1deb
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-09 00:50:30 +0000

    task 10: added

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) <= 0:
+        return None
+    new_list = []
+    for x in my_list:
+        new_list.append(True if x % 2 == 0 else False)
+    return new_list
