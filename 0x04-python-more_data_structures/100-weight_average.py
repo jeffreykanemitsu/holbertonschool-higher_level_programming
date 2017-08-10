69f595f22c27a1f7ea64ab2ec60d1ea275b60e91
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 19:47:10 +0000

    task 100: changed == to is. oops

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None or if len(my_list) == 0:
+    if my_list is None or len(my_list) is 0:
         return 0
     return (sum([x*y for x, y in my_list]) / sum([y for x, y in my_list]))
