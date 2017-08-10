ccbfd2bc55b746a2b9697b040f97ce8bdbfe06e6
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 10:19:32 +0000

    task 100: fixed syntax error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,4 @@
 def weight_average(my_list=[]):
     if my_list is None:
         return 0
-    return (sum[dend*sor for dend, sor in my_list]) / sum([sor for dend, sor in my_list])
-
-#dend = dividend
-#sor = divisor
+    return (sum([dend*sor for dend, sor in my_list]) / sum([sor for dend, sor in my_list]))
