57a3818504131283f91097310a2173182f23b4d0
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 19:48:28 +0000

    task 10: added my_dict is 0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict is None or my_dict is 0:
         return None
     sorted_dict = sorted(my_dict)
     return(sorted_dict[-1])
