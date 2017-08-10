1fb70d24072e9a242695027fab20977d3e92df5e
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 20:01:00 +0000

    task 10: rewrote code because of annoying checker error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(my_dict):
     if my_dict is None or my_dict is 0:
         return None
-    sorted_dict = sorted(my_dict)
-    return(sorted_dict[-1])
+    bestscore = max(my_dict, key=my_dict.get)
+    return bestscore
