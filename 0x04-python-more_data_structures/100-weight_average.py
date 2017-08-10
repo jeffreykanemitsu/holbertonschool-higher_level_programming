7da7fe85574aaa28df561c825a2a2d7564567b99
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 19:45:34 +0000

    task 100: added if len(my_dict) == 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list is None or if len(my_list) == 0:
         return 0
     return (sum([x*y for x, y in my_list]) / sum([y for x, y in my_list]))
