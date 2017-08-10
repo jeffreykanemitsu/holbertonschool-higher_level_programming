c09fa32f34af2fbe67ba86f4b80a5850b363717c
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 20:06:29 +0000

    task 10: changed to one line :D

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None or my_dict is 0:
-        return None
-    bestscore = max(my_dict, key=my_dict.get)
-    return bestscore
+    return (max(my_dict, key=my_dict.get) if my_dict else None)
