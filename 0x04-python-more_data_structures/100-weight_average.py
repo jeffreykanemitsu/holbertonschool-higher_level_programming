eb071c4a67ef46ca6cb01e2a6938ddd047b3eb3e
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-10 19:43:33 +0000

    task 100: line 5 was too long

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,4 @@
 def weight_average(my_list=[]):
     if my_list is None:
         return 0
-    return (sum([dend*sor for dend, sor in my_list]) / sum([sor for dend, sor in my_list]))
+    return (sum([x*y for x, y in my_list]) / sum([x for y, y in my_list]))
