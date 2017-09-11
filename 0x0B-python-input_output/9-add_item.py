c1ee3989ecbde6299ef638a3d247cf944420593c
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-11 07:08:25 +0000

    task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,7 +4,6 @@
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-my_list = []
 
 fn = "add_item.json"
 
