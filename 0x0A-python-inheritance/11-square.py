b554d181b8cf1683e748a4d1b1a758de90a698a8
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-11 07:03:23 +0000

    Task 11: forgot to import 9-rectangle -_-

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 
+Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
@@ -8,9 +11,7 @@ class Square(Rectangle):
         self.__size = size
 
     def area(self):
-        a = self.__size * self.__size
-        return a
+        return self.__size ** 2
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
-    
