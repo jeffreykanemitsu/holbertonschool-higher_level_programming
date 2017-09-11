2b52a954eff7d10c353feb8bc17cb7f9c1abb53d
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-11 06:52:38 +0000

    task 10 and 11: added

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,13 +6,14 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 my_list = []
 
+fn = "add_item.json"
 
 try:
-    my_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
+    my_list = load_from_json_file(fn)
+except:
     my_list = []
 
 for i in argv[1:]:
     my_list.append(arg)
 
-save_to_json_file(my_list, 'add_item.json')
+save_to_json_file(my_list, fn)
