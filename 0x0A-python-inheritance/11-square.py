4287fb151f47228d5a0654b09726770cafa64ab9
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-11 06:59:30 +0000

    task 11: added def area(self)

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,8 +6,13 @@ def __init__(self, size):
 
 class Square(Rectangle):
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        a = self.__size * self.__size
+        return a
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
