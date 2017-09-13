a53e35e8e05720709c9523d01599bb4f2350e53e
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-13 00:55:30 +0000

    moved imports

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from models.base import Base
 '''
 Module for the Rectangle class
 '''
+
+
+from models.base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from rectangle import Rectangle
 '''
 Module for the Square class
 '''
+
+
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
