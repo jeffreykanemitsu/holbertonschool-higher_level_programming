7b81b301c3eac496a8dcc1126dcb2f5b3e83e2a3
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-10-17 00:36:53 +0000

    task 0-12: added

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- write a script that lists all databases of your MySQL server
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- write a script that creates the database hbtn_0c_0 in your MySQL server
+-- if the database hbtn_0c_0 already exists, your script shot not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
