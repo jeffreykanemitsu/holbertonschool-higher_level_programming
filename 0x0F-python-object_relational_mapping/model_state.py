49fe423596c3c38ac420daefa05597038e6a58de
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-11-10 10:11:25 +0000

    task 6: model_state.py: fixed pep8 error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,15 +4,19 @@ Contains the class definition of a State and
 and instance Base = declarative_base():
 '''
 
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     '''
     defines State class
     '''
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True,
+                unique=True)
     name = Column(String(128), nullable=False)
