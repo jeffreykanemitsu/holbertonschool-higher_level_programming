3e4d9019d668af212b59e241a29fe42b3828430b
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-11-10 11:04:19 +0000

    task 7: changed spacing

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,16 +3,16 @@
 lists all State objects from the database hbtn_0e_6_usa
 '''
 
-
 if __name__ == "__main__":
     from sys import argv
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                          (argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    ngin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                     argv[2],
+                                                                     argv[3]))
+    Base.metadata.create_all(ngin)
+    Session = sessionmaker(bind=ngin)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
