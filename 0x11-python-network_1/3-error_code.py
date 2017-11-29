18560a8851d7800de9cc7a1ef7853f620765b02f
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-11-29 07:25:12 +0000

    task 3: changed error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.URLError as e:
+    except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
